1277b41de0b077c9100a6d221eb72a307c3f6e1d
dimitriwashere <dimitriwashere@gmail.com>
2017-03-22 21:42:07 -0400

    Add api versioning

---
+++ app/controllers/api/v1/people_controller.rb
@@ -0,0 +1,44 @@
+module Api::V1
+  class PeopleController < ApiController
+    before_action :set_person, only: [:show, :update, :destroy]
+
+    # GET /people
+    def index
+      @people = Person.all
+      json_response(@people)
+    end
+
+    # POST /people
+    def create
+      @person = Person.create!(person_params)
+      json_response(@person, :created)
+    end
+
+    # GET /people/:id
+    def show
+      json_response(@person)
+    end
+
+    # PUT /people/:id
+    def update
+      @person.update(person_params)
+      head :no_content
+    end
+
+    # DELETE /people/:id
+    def destroy
+      @person.destroy
+      head :no_content
+    end
+
+    private
+
+    def set_person
+      @person = Person.find(params[:id])
+    end
+
+    def person_params
+      params.permit(:name, :favoriteCity)
+    end
+  end
+end
